7567fd13ec05836ca2aacfaf9111a983461c8d2a
kapilmunjewar <kapilmunjewar88@gmail.com>
2019-10-18 06:45:23 -0700

    Updates

---
+++ precache-manifest.7268ba9e1cc79288915bf30dc7d51e5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67669b665e49a28030a82b2889f7051d",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "8f43d398f6b2db214291",
+    "url": "/pics/static/css/main.310e5213.chunk.css"
+  },
+  {
+    "revision": "c0d011b3df481c51a6d9",
+    "url": "/pics/static/js/2.95aadcb8.chunk.js"
+  },
+  {
+    "revision": "8f43d398f6b2db214291",
+    "url": "/pics/static/js/main.09627dc8.chunk.js"
+  },
+  {
+    "revision": "0ec7b01002faefe50b45",
+    "url": "/pics/static/js/runtime-main.f76586c7.js"
+  }
+]);
